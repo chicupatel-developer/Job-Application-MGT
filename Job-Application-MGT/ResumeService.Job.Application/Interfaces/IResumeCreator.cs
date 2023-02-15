3345beec089ea625c97780d946dfa7858b698122
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2023-02-15 11:50:20 -0600

    resume-create-wip1

---
+++ Job-Application-MGT/ResumeService.Job.Application/Interfaces/IResumeCreator.cs
@@ -0,0 +1,25 @@
+﻿using SelectPdf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ResumeService.Job.Application.Models;
+using EF.Core.Job.Application.Models;
+
+namespace ResumeService.Job.Application.Interfaces
+{
+    public interface IResumeCreator
+    {
+        HtmlToPdf GetHtmlToPdfObject();
+        string GetPageHeader();
+        string GetPersonalInfoString(PersonalInfo personalInfo);
+        string GetTechnicalSkillsString(List<string> skills);
+        string GetPageFooter();
+        string GetWorkExperienceString(List<WorkExperience> workExperience);
+        string GetEducationString(List<Education> educations);
+        bool AddUserDataWhenResumeCreated(UserResumeCreate userData);
+        IEnumerable<UserResumeCreate> GetUserResumeCreateData();
+        bool AddUserDataWhenResumeEmailed(UserResumeEmail userData);
+        IEnumerable<UserResumeEmail> GetUserResumeEmailData();
+    }
+}
