bb25b555eda9dda03cf23a3ddd2322de075c60ab
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2023-02-15 13:05:09 -0600

    resume-create-wip2

--- Job-Application-MGT/API.Job.Application/Controllers/ResumeCreatorController.cs
+++ Job-Application-MGT/API.Job.Application/Controllers/ResumeCreatorController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EF.Core.Job.Application.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ServiceLib.Job.Application.DTO;
+using ServiceLib.Job.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Web;
+using ResumeService.Job.Application.Interfaces;
+
 
 namespace API.Job.Application.Controllers
 {
@@ -11,5 +20,15 @@ namespace API.Job.Application.Controllers
     [ApiController]
     public class ResumeCreatorController : ControllerBase
     {
+        private APIResponse _response;
+
+        private readonly IResumeCreator _resumeCreator;
+
+        public ResumeCreatorController(IResumeCreator resumeCreator)
+        {
+            _resumeCreator = resumeCreator;
+        }
+
+
     }
 }
